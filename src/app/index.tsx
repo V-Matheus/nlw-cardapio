416e8908baa21861d5800134b6e84852d15c33a9
Victor Matheus <victormatheus507@gmail.com>
2024-02-08 15:56:16 -0300

    criando categorias

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, FlatList } from 'react-native'
+import { CATEGORIES } from '@/utils/data/products'
 import { Header } from '@/components/Header'
+import { CategoryButton } from '@/components/category-button'
+
 
 export default function Home() {
+
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelected(selectedCategory: string) {
+
+    setCategory(selectedCategory)
+  }
+
   return (
     <View className=' flex-1 pt-8'>
-      <Header title='Faça seu pedido' carQuantityItems={1} />
+      <Header title='Faça seu pedido' carQuantityItems={3} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item} renderItem={({ item }) => <CategoryButton title={item} onPress={() => handleCategorySelected(item)} isSelected={ item === category} />}
+        horizontal
+        className='max-h-10 mt-5'
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }} />
     </View>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,8 +13,8 @@ export const Header = ({ title, carQuantityItems = 0 }: HeaderProps) => {
     <View className='flex-row items-center border-b border-s-slate-700 pb-5 mx-5'>
       <View className='flex-1 '>
         <Image source={require('../assets/logo.png')} className='h-6 w-32' />
+        <Text className='text-white text-xl font-heading mt-2'>{title}</Text>
       </View>
-      <Text className='text-white text-xl font-heading mt-2'>{title}</Text>
 
       {carQuantityItems > 0 &&
         <TouchableOpacity className='relative' activeOpacity={0.7}>
