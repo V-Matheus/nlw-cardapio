d715eab4fc15be7eb9ecd24862f30ada74f1281d
Victor Matheus <victormatheus507@gmail.com>
2024-02-08 14:49:28 -0300

    configurando fonts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,8 +3,8 @@ import { View, Text, StyleSheet } from 'react-native'
 
 export default function Home() {
   return (
-    <View className='bg-slate-900 flex-1'>
-      <Text className='text-white' >Hello react Native.</Text>
+    <View className=' flex-1 pt-8'>
+      <Text className='text-white text-2xl' >Hello react Native.</Text>
     </View>
   )
 }
