7b1bb9ce3a6c2b39915ae8f6bb5c9fab7cddc0cc
Victor Matheus <victormatheus507@gmail.com>
2024-02-09 10:59:16 -0300

    adicionar produto no carrinho

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,9 +1,25 @@
 import { View } from "react-native";
 
+import { Header } from "@/components/Header";
+import {Product} from '@/components/product'
+
+import { useCardStore } from "@/stores/cart-store";
+
 export default function Cart() {
+
+  const cartStore = useCardStore()
+
   return (
     <View className="flex-1 pt-8">
+      <Header title="Seu Carrinho" />
 
+      <View className="p-5 flex-1">
+        {
+          cartStore.products.map((product) => (
+            <Product key={product.id} data={product} />
+          ))
+        }
+      </View>
     </View>
   )
 }
