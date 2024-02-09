a75129b4d2f5937a761598250f127726672a68f1
Victor Matheus <victormatheus507@gmail.com>
2024-02-09 12:21:35 -0300

    salvando os dados no asyncStorage

--- src/stores/cart-store.tsx
+++ src/stores/cart-store.tsx
@@ -1,5 +1,7 @@
 import { ProductProps } from '@/utils/data/products'
 import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import * as cardInMemory from './helpers/cart-in-memory'
 
 export type ProductCartProps = ProductProps & {
@@ -12,13 +14,17 @@ type StateProps = {
   remove: (productId: string) => void
 }
 
-export const useCardStore = create<StateProps>((set) => ({
-  products: [],
-  add: (produc: ProductProps) => set((state) => ({
-    products: cardInMemory.add(state.products, produc)
-  })),
+export const useCardStore = create(
+  persist<StateProps>((set) => ({
+    products: [],
+    add: (produc: ProductProps) => set((state) => ({
+      products: cardInMemory.add(state.products, produc)
+    })),
 
-  remove: (productId: string) => set((state) => ({
-    products: cardInMemory.remove(state.products, productId)
+    remove: (productId: string) => set((state) => ({
+      products: cardInMemory.remove(state.products, productId)
+    }))
+  }), {
+    name: 'nlw-cardapio:cart',
+    storage: createJSONStorage(() => AsyncStorage)
   }))
-}))
