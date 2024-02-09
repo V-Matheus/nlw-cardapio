04ac962214d1b416f6af63d99146cfaedf705f45
Victor Matheus <victormatheus507@gmail.com>
2024-02-09 14:03:31 -0300

    formando mensagens

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, Text, ScrollView, Alert } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
@@ -13,7 +14,7 @@ import { Button } from "@/components/button";
 import { LinkButton } from "@/components/link-button";
 
 export default function Cart() {
-
+  const [address, setAddress] = useState('')
   const cartStore = useCardStore()
 
   const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.price * product.quantity, 0))
@@ -28,6 +29,26 @@ export default function Cart() {
         onPress: () => cartStore.remove(product.id),
       }
     ])
+  }
+
+  function handleOrder() {
+    if (address.trim().length === 0) {
+      return Alert.alert('Pedido', 'Informe os dados da entrega.')
+    }
+
+    const products = cartStore.products.map((product) =>
+      `\n${product.quantity} ${product.title}`
+    ).join('')
+
+    const mensage = `
+    🍔 NOVO PEDIDO
+      \n Entregar em: ${address}
+      ${products}
+      \n Valor Total: ${total}
+    `
+
+    console.log(mensage);
+    
   }
 
   return (
@@ -54,13 +75,15 @@ export default function Cart() {
               <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
             </View>
 
-            <Input placeholder='Informe o endereço de entrega com rua, bairro, CEP, número e complemento...' />
+            <Input
+              placeholder='Informe o endereço de entrega com rua, bairro, CEP, número e complemento...'
+              onChangeText={setAddress} />
           </View>
         </ScrollView>
       </KeyboardAwareScrollView>
 
       <View className="p-5 gap-5">
-        <Button>
+        <Button onPress={handleOrder}>
           <Button.Text>Enviar pedido</Button.Text>
           <Button.Icon><Feather name="arrow-right-circle" size={20} /></Button.Icon>
         </Button>
